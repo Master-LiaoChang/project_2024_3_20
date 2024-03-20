c96709eeab44a4ba99744eb7aad9d0f1453abd5e
liao_chang <1483934051@qq.com>
2024-03-20 22:27:53 +0800

    添加项目文件。

---
+++ 源.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//寒假班——函数
+//库函数 相关头文件 https://zh.cppreference.com/w/c/header
+//慢慢学库函数
+
+//库函数的使用方法
+//cplusplus.com https://legacy.cplusplus.com/reference/clibrary/
+//举例 sqrt 开方
+//double sqrt (double x);
+
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	double r = sqrt(16);
+//	printf("%lf\n", r);
+//	return 0;
+//}
+
+//使用sqrt函数进行开方，其中调用了math.h头文件
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	double d = 16.0;
+//	double r = sqrt(16);
+//	printf("%lf\n", r);
+//	return 0;
+//}
+
+
+//自定义函数,根据实际的需要自己设计和实现、使用的函数
+//形式如下
+//ret_type fun_name()
+// {
+// 
+// }
+// 
+//返回类型  函数名（形式参数）
+//{} 大括号里为函数体
